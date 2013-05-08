b3c61a1a167f8d86da377b643376926f2c3355ed
Jay Vilalta <jay.vilalta@gmail.com>
2013-05-07 19:19:53 -0500

    initial commit

---
+++ Lesson1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Job;
+
+namespace Lesson1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Create the scheduler factory
+			ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+
+			//Ask the scheduler factory for a scheduler
+			IScheduler scheduler = schedulerFactory.GetScheduler();
+
+			//Start the scheduler so that it can start executing jobs
+			scheduler.Start();
+
+			// Create a job of Type WriteToConsoleJob
+			IJobDetail job = JobBuilder.Create(typeof(WriteToConsoleJob)).WithIdentity("MyJob", "MyJobGroup").Build();
+
+			//Schedule this job to execute every second, a maximum of 10 times
+			ITrigger trigger = TriggerBuilder.Create().WithSchedule(SimpleScheduleBuilder.RepeatSecondlyForTotalCount(10)).StartNow().WithIdentity("MyJobTrigger", "MyJobTriggerGroup").Build();
+			scheduler.ScheduleJob(job, trigger);
+
+			//Wait for a key press. If we don't wait the program exits and the scheduler gets destroyed
+			Console.ReadKey();
+
+			//A nice way to stop the scheduler, waiting for jobs that are running to finish
+			scheduler.Shutdown(true);
+		}
+	}
+}
---
+++ Lesson1/WriteToConsoleJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Lesson1
+{
+	class WriteToConsoleJob : IJob
+	{
+		public void Execute(IJobExecutionContext context)
+		{
+			Console.WriteLine("Execute method for job {0} in group {1} called at {2}", context.JobDetail.Key.Name, context.JobDetail.Key.Group, DateTime.Now);
+			Console.WriteLine("Trigger {0} in group {1} was fired", context.Trigger.Key.Name, context.Trigger.Key.Group);
+		}
+	}
+}
